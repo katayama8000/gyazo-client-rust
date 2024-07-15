bcf5505618333a3a84a4c1c132c95ce555b1b465
katayama800 <tattu.0310@gmail.com>
2024-07-15 09:15:37 +0000

    Public UploadParamsBuilder

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ mod gyazo_client;
 
 pub use gyazo_client::{
     DeleteImageResponse, GyazoClient, GyazoError, GyazoImageResponse, ImageMetadata, ImageOcr,
-    UploadImageResponse, UploadParams,
+    UploadImageResponse, UploadParams, UploadParamsBuilder,
 };
