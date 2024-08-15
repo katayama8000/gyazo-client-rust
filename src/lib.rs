a8a4f28ef1cc1f1f40fa970a4f735b4a51ecb521
katayama800 <tattu.0310@gmail.com>
2024-08-15 14:57:19 +0000

    Bump gyazo_client version to 0.2.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod gyazo_client;
 
 pub use gyazo_client::{
-    DeleteImageResponse, GyazoClient, GyazoError, GyazoImageResponse, ImageMetadata, ImageOcr,
-    OembedResponse, UploadImageResponse, UploadParamsBuilder,
+    DeleteImageResponse, GyazoClient, GyazoClientOptions, GyazoError, GyazoImageResponse,
+    ImageMetadata, ImageOcr, OembedResponse, UploadImageResponse, UploadParamsBuilder,
 };
